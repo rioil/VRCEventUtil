8ccfb21a96a85e9f5488088e87f94b257643f695
rioil <rioil.dev@gmail.com>
2021-10-06 22:29:53 +0900

    設定画面を追加

---
+++ VRCEventUtil/ViewModels/SettingWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Livet;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VRCEventUtil.ViewModels
+{
+    class SettingWindowViewModel : ViewModel
+    {
+        public void Initialize()
+        {
+
+        }
+    }
+}
