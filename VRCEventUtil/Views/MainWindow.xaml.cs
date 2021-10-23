63ad89691e7cdc61b46f5e869e42d1792b8cc8ec
rioil <rioil.dev@gmail.com>
2021-10-23 14:09:32 +0900

    MahApps.Metroを導入

--- VRCEventUtil/Views/MainWindow.xaml.cs
+++ VRCEventUtil/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,7 +22,7 @@ namespace VRCEventUtil.Views
      *
      * Those events are managed using WeakEventListener, so it is not occurred memory leak, but you should release explicitly.
      */
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
